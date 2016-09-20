2110197311a538d50345dc2859fd1696c07a42fc
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-09-20 20:29:24 +0900

    RSS関連のカスタマイズ

--- functions.php
+++ functions.php
@@ -19,3 +19,39 @@ function my_comment_form_default_fields($args){
 	$args['url'] = ''; 		// 「ウェブサイト」を削除
 	return $args;
 }
+
+/**
+ * head内にRSSのlink要素を出力する
+ */
+add_theme_support('automatic-feed-links');
+
+/**
+ * 本文から抜粋として切り出す文字数を指定
+ */
+add_filter('excerpt_mblength', 'my_excerpt_mblength');
+function my_excerpt_mblength($length){
+	return 50;
+}
+
+/**
+ * 残りの部分がある旨の表示を変更
+ */
+add_filter('excerpt_more', 'my_excerpt_more');
+function my_excerpt_more($more){
+	return '[......]';
+}
+/**
+ * rssフィードにアイキャッチ画像を追加
+ */
+function rss_post_thumbnail( $content) {
+    global $post;
+    if (has_post_thumbnail( $post->ID)) {
+        $content = '<p>' . get_the_post_thumbnail($post->ID) .'</p>' . $content;
+    }
+    return $content;
+}
+add_filter( 'the_excerpt_rss',  'rss_post_thumbnail');
+add_filter( 'the_content_feed', 'rss_post_thumbnail');
+
+// RSS 2.0を停止
+//remove_action('do_feed_rss2', 'do_feed_rss2', 10, 1);

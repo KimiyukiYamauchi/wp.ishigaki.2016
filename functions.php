26ad3b0536d55b349d5ac6b1816d9507eaf66cf4
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-09-20 20:28:36 +0900

    コメント投稿フォームから入力フィールドを削除

--- functions.php
+++ functions.php
@@ -8,3 +8,14 @@ add_theme_support('post-thumbnails');
  * カスタムメニュー機能を使用可能にする
  */
 add_theme_support('menus');
+
+/**
+ * コメント投稿フォームから入力フィールドを削除する
+ */
+add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
+function my_comment_form_default_fields($args){
+	//$args['author'] = '';	// 「名前」を削除
+	//$args['email'] = '';	// 「メールアドレス」を削除
+	$args['url'] = ''; 		// 「ウェブサイト」を削除
+	return $args;
+}
